3ac7e3cfd870ccce3f1bc28ff4c4358a5c5bb01d
davi-pou07 <davihareis@gmail.com>
2021-01-18 18:00:33 -0300

    futuro

--- Produtos/produtoController.js
+++ Produtos/produtoController.js
@@ -8,23 +8,28 @@ router.get("/admin/produtos/novo",(req, res) => {
     res.render("admin/produtos/new");
 });
 
-// router.post("/produto/save", (req, res) => {
-//     var nome = req.body.nome;
-//     var descricao = req.body.descricao;
-//     var preco = req.body.preco;
-    
-//     if(title != undefined){
-        
-//         Category.create({
-//             title: title,
-//             slug: slugify(title)
-//         }).then(() => {
-//             res.redirect("/admin/categories");
-//         })
-
-//     }else{
-//         res.redirect("/admin/categories/new");
-//     }
-// });
+router.post("/produto/save", (req, res) => {
+    var nome = req.body.nome;
+    var descricao = req.body.descricao;
+    var preco = req.body.preco;
+    var quantidade = req.body.quantidade;
+    var codBar = req.body.codBar;
+    var status = req.body.status
+    if(nome || preco || quantidade || status != undefined ){
+	Produto.create({
+		nome:nome,
+		descricao:descricao,
+		preco:preco,
+		quantidade:quantidade,
+		codBar:codBar,
+		status:status,
+		slug:slugify(nome),
+	}).then(()=>{
+		res.redirect("/admin/produtos")
+	})
+    }else{
+        res.redirect("/admin/produtos/novo")
+    }
+});
 
 module.exports = router;
