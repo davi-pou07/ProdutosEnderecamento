1932813a88f3d5977e6c8e1182238d2d367eaa9c
davi-pou07 <davihareis@gmail.com>
2021-02-24 22:28:07 -0300

    babujando tudo

--- DataBases/databases.js
+++ DataBases/databases.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('produtosEnderecamento','root','davi6259',{
-    host:'localhost',
+const connection = new Sequelize('heroku_9b63551fa2f9a8f','b4be845f37ea68','295062e4',{
+    host:'us-cdbr-east-03.cleardb.com',
     dialect: 'mysql',
     //configurando timezone
     timezone: "-03:00",
     logging:false //Remove log do terminal
 })
-
+// mysql://b4be845f37ea68:295062e4@us-cdbr-east-03.cleardb.com/heroku_9b63551fa2f9a8f?reconnect=true
 module.exports = connection;
