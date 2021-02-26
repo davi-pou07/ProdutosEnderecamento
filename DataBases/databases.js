21ca7b92d7217e6284906503f706e5227c3b549a
davi-pou07 <davihareis@gmail.com>
2021-02-26 14:56:28 -0300

    databases

--- DataBases/databases.js
+++ DataBases/databases.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('produtosEnderecamento','postgres','davi6259',{
-    host:'localhost',
+const connection = new Sequelize('d3918lsvq94fe','kqainbgfpqbbzz','34d884cbb7a86c957cf388c99ea462f2c2d7616094f9b753ea59de093c21031f',{
+    host:'ec2-34-203-255-149.compute-1.amazonaws.com',
     dialect: 'postgres',
     //configurando timezone
     timezone: "-03:00",
