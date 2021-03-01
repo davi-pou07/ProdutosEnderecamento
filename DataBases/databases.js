890ae096c763a390b9a39b25ee6eea88b5bc1260
davi-pou07 <davihareis@gmail.com>
2021-03-01 15:26:37 -0300

    Comentarios

--- DataBases/databases.js
+++ DataBases/databases.js
@@ -9,3 +9,14 @@ const connection = new Sequelize('da1i1bcmrp89ss','ttsgwultjbdglu','d1e43edbcf38
 })
 // mysql://b4be845f37ea68:295062e4@us-cdbr-east-03.cleardb.com/heroku_9b63551fa2f9a8f?reconnect=true
 module.exports = connection;
+
+//https://help.heroku.com/DR0TTWWD/seeing-fatal-no-pg_hba-conf-entry-errors-in-postgres
+//https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+//https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
+
+//https://medium.com/@ochieng.grace/sequelize-your-way-to-heroku-with-express-2c31be3752e0
+
+
+//heroku config:set PGSSLMODE=no-verify
+//heroku pg:reset DATABASE --confirm produtosenderecamento
+//set DATABASE_URL=postgres://$(whoami)
